9bec8ea5964abcc91e2359d8e737771fb609a4e5
manish kumar singh <39149566+manish25297@users.noreply.github.com>
2018-06-10 19:20:32 +0530

    matplot

---
+++ matplot lib/plot(x,y).py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3]
+y=[4,5,6]
+
+x2=[7,8,9]
+y2=[4,5,6]
+
+plt.plot(x,y,color="cyan",label="population density")
+plt.plot(x2,y2,color="red",label="city", linewidth=2)
+plt.title("hellO matplot")  # for title naming of the graph
+plt.xlabel("population")     #naming the x axis
+plt.ylabel("year")            #naming the y axis
+plt.legend()# for showing the labels of the plot
+plt.grid(color='green')
+plt.show()                  #for showing the graph 
---
+++ matplot lib/plt.scatter().py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5,6,7,8]
+y=[5,2,4,2,1,4,5,2]
+
+plt.scatter(x,y,color="red",label="skitcat",s=50,marker="o") # s=square,p=pentagon,o=circle
+#marker style - h=hexagon,o=circle,s=square, p=pentagon
+# s= size for the marker 
+plt.show()
