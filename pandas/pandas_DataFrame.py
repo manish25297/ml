935e227830aed970057bb6349fda30127189abca
manish kumar singh <manish.25297@gmail.com>
2018-07-06 21:18:00 +0530

    Add files via upload

--- pandas/pandas_DataFrame.py
+++ pandas/pandas_DataFrame.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 name=["manish","aman","rakesh","ashok","dakshit"]
@@ -6,6 +7,17 @@ percentage=[70,71,72,73,74]
 
 joined=zip(name,roll,percentage)
 #print joined
+"""for i,j,k in zip(name,roll,percentage):
+  print i,j,k"""
 
 activity= pd.DataFrame(joined,index=range(5,10),columns=['name','roll','percentage'])
 print activity
+
+
+"""for i,j,k,l in [(0,1,2,3),(4,5,6,7)]:
+	print i+j
+
+
+   for i,j in [[0,4],[4,6]]:
+	i+j
+"""
